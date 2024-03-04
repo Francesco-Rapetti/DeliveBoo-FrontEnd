b8640e0d1a3409fa0020bc239fd3412f3211bcd1
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-04 10:53:40 +0100

    Create header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from 'axios'; //importo Axios
 import { store } from "./store.js" //state management
+import AppHeader from './components/AppHeader.vue'
 
 export default {
+	components: {
+		AppHeader
+	},
 	data() {
 		return {
 			store
@@ -18,7 +22,12 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+	<header>
+		<AppHeader />
+	</header>
+	<router-view></router-view>
+</template>
 
 <style lang="scss">
 // importo il foglio di stile generale dell'applicazione, non-scoped
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<script></script>
+
+<template>
+    questo è un header
+</template>
+
+<style lang="scss" scoped>
+</style>
