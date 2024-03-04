0e7f7a48736f7db840a9888357afb139becceaa4
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-04 18:29:21 +0100

    Fix cart page

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -34,21 +34,24 @@ export default {
 </script>
 
 <template>
-    <h1>Carrello</h1>
-    <button class="btn btn-primary" @click="emptyCart()">Svuota</button>
-    <p v-if="!store.cart || !store.cart.length">Non ci sono elementi nel carrello</p>
-    <p v-else>Il carrello contiene {{ store.cart.length }} elementi</p>
-    <p v-for="dish in store.cart">{{ dish.name }}</p>
-
     <div class="container">
-        <div class="row">
-            <div class="col-3" v-for="dish in store.dishList">
-                <div class="card" style="width: 18rem;">
-                    <img :src="dish.img" class="card-img-top" :alt="dish.name + ' image'">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ dish.name }}</h5>
-                        <p class="card-text">{{ dish.price }}</p>
-                        <a href="#" class="btn btn-primary" @click="addItemToCart(dish)">Aggiungi</a>
+
+        <h1>Carrello</h1>
+        <button class="btn btn-primary" @click="emptyCart()">Svuota</button>
+        <p v-if="!store.cart || !store.cart.length">Non ci sono elementi nel carrello</p>
+        <p v-else>Il carrello contiene {{ store.cart.length }} elementi</p>
+        <p v-for="dish in store.cart">{{ dish.name }}</p>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-3" v-for="dish in store.dishList">
+                    <div class="card" style="width: 18rem;">
+                        <img :src="dish.img" class="card-img-top" :alt="dish.name + ' image'">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ dish.name }}</h5>
+                            <p class="card-text">{{ dish.price }}</p>
+                            <a href="#" class="btn btn-primary" @click="addItemToCart(dish)">Aggiungi</a>
+                        </div>
                     </div>
                 </div>
             </div>
