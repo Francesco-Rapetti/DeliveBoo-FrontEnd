27100b0c71ea7be40126467fd1f5e9235c43f4b7
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-08 16:58:11 +0100

    Implement payment

--- src/pages/PaymentPage.vue
+++ src/pages/PaymentPage.vue
@@ -1,58 +1,38 @@
-<template>
-    <div class="sub-body">
-        <header class="header">
-            <h1>Totale Ordine</h1>
-            <h2 class="price"><span class="price__dollar">$</span>173<span class="price__time"></span>
-            </h2>
-            <p class="desc">Procedi al Pagamento</p>
-        </header>
-
-        <div class="pay-select">
-            <div class="pay-select__item pay-select--card is-active" @click="selectPaymentMethod('card')">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/346994/Card%20Icon.svg" alt="" />
-                <p>Debit/Credit Card</p>
-            </div>
-
-
-
-
-        </div>
-
-        <div class="select-body">
-            <div class="select-body__content select-body--card is-active">
-                <form action="/" method="POST" id="cardForm">
-                    <label class="form__label" for="card-number">Card Number</label>
-                    <div class="card-input" id="card-number"></div>
-
-                    <label class="form__label" for="expiration-month">Expiration Date</label>
-                    <div class="date__container">
-                        <div class="card-input" id="expiration-month"></div>
-                        <div class="card-input" id="expiration-year"></div>
-                    </div>
-
-                    <label class="form__label" for="cvv">CVV</label>
-                    <div class="card-input" id="cvv"></div>
-
-                    <label class="form__label" for="cvv">Billing Zip Code</label>
-                    <div class="card-input" id="postal-code"></div>
-
-                    <input type="submit" value="Subscribe" id="submit" />
-                </form>
-            </div>
-
-            <div class="select-body__content select-body--paypal">
-
-            </div>
-        </div>
-    </div>
-</template>
-
 <script>
 import braintree from 'braintree-web';
+import { store } from "../store.js";
+import axios from 'axios';
 
 export default {
+    name: "PaymentPage",
+    data() {
+        return {
+            store,
+            order: null,
+            total: 0,
+        }
+    },
+
+    beforeRouteEnter(to, from, next) {
+        if (from.name !== 'credentials') {
+            next({ path: '/' })
+        } else {
+            next()
+        }
+    },
+
     mounted() {
         this.initializeBraintree();
+        this.order = JSON.parse(localStorage.order);
+        this.total = this.order.total;
+        console.log(this.order.total);
+
+        this.$router.beforeEach((to, from) => {
+            if (from.name === 'payment' && to.name !== 'confirmed' && to.name !== 'rejected') {
+                alert('Non puoi abbandonare la pagina adesso')
+                return false
+            }
+        })
     },
     methods: {
         initializeBraintree() {
@@ -114,6 +94,7 @@ export default {
                 }, (err, hostedFields) => {
                     if (err) {
                         console.error(err);
+
                         return;
                     }
 
@@ -121,14 +102,26 @@ export default {
 
                     form.addEventListener('submit', (event) => {
                         event.preventDefault();
-                        alert('Fancy submission here');
+                        // alert('Fancy submission here');
                         hostedFields.tokenize((err, payload) => {
                             if (err) {
                                 console.error(err);
+                                localStorage.orderErrorMessage = err.message;
+                                axios.post(this.store.urlAPI + '/orders/' + this.order.id, { status: 'rejected' })
+                                    .then((response) => {
+                                        console.log(response.data);
+                                    });
+                                this.$router.push({ name: 'rejected' });
                                 return;
+                            } else {
+                                axios.post(this.store.urlAPI + '/orders/' + this.order.id, { status: 'accepted' })
+                                    .then((response) => {
+                                        console.log(response.data);
+                                    });
+                                console.log('Got a nonce: ' + payload.nonce);
+                                this.$router.push({ name: 'confirmed' });
                             }
 
-                            console.log('Got a nonce: ' + payload.nonce);
                         });
                     });
                 });
@@ -156,7 +149,65 @@ export default {
 };
 </script>
 
+<template>
+    <div id="payment-conainer" class="container d-flex flex-column justify-content-center align-items-center">
+
+        <div class="sub-body">
+            <header class="header">
+                <h1>Totale Ordine</h1>
+                <h2 class="price"><span class="price__dollar">€</span>{{ total }}<span class="price__time"></span>
+                </h2>
+                <p class="desc">Procedi al Pagamento</p>
+            </header>
+
+            <div class="pay-select">
+                <div class="pay-select__item pay-select--card is-active" @click="selectPaymentMethod('card')">
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/346994/Card%20Icon.svg" alt="" />
+                    <p>Debit/Credit Card</p>
+                </div>
+
+
+
+
+            </div>
+
+            <div class="select-body">
+                <div class="select-body__content select-body--card is-active">
+                    <form action="/" method="POST" id="cardForm">
+                        <label class="form__label" for="card-number">Card Number</label>
+                        <div class="card-input" id="card-number"></div>
+
+                        <label class="form__label" for="expiration-month">Expiration Date</label>
+                        <div class="date__container">
+                            <div class="card-input" id="expiration-month"></div>
+                            <div class="card-input" id="expiration-year"></div>
+                        </div>
+
+                        <label class="form__label" for="cvv">CVV</label>
+                        <div class="card-input" id="cvv"></div>
+
+                        <label class="form__label" for="cvv">Billing Zip Code</label>
+                        <div class="card-input" id="postal-code"></div>
+
+                        <input type="submit" value="Subscribe" id="submit" />
+                    </form>
+                </div>
+
+                <div class="select-body__content select-body--paypal">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
 <style scoped>
+#payment-conainer {
+    height: 90vh;
+}
+
 /* ----- GENERAL */
 html {
     height: 100%;
