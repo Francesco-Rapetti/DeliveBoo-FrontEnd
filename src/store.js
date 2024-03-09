56c0dcd4531ee326358b94b4899ea7a0481115fd
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-09 15:12:01 +0100

    Add urlBackend

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
+    urlBackend: "http://127.0.0.1:8000",
     urlAPI: "http://127.0.0.1:8000/api",
     apiRestaurantEndpoint: "/restaurants",
     apiFoodTypeEndpoint: "/types",
