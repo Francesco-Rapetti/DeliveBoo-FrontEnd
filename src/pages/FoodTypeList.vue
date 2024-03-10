25560ed1b40c96174dad91e14692ac1cf7c9f727
PinoGabriel <pinogabriel01@gmail.com>
2024-03-10 14:24:36 +0100

    bug fix

--- src/pages/FoodTypeList.vue
+++ src/pages/FoodTypeList.vue
@@ -53,9 +53,10 @@ export default {
                 }
             });
         },
-    },
-    computed: {
-
+        selectFoodType(foodType) {
+            this.selectedFoodType = foodType.name;
+            this.$router.push({ name: 'restaurants', query: { foodType: this.selectedFoodType } });
+        },
     },
 };
 </script>
