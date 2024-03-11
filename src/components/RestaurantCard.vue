2956239853e7c99ceeb3808827dc198dffac19b8
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-11 14:06:30 +0100

    Implement search and minor style changes

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -12,43 +12,47 @@ export default {
 </script>
 
 <template>
-    <div class="col-md-4 gy-4">
-        <div class="card glass h-100">
-            <div class="card-img-container" type="button" data-bs-toggle="modal" data-bs-target="#dishInfo">
+    <div class="card glass p-0">
+        <div class="card-body d-flex flex-column justify-content-between align-items-center">
+            <div class="card-img-container" type="button" data-bs-toggle="modal" data-bs-target="#restaurantInfo"
+                @click="store.currentRestaurant = item">
                 <img :src="item.img.substring(0, 4) === 'http' ? item.img : store.urlBackend + '/storage/' + item.img"
                     class="card-img-top" alt="Restaurant Image">
             </div>
-            <div class="card-body d-flex flex-column justify-content-between align-items-center">
-                <div class="w-100">
-                    <h5 class="card-title text-center">{{ item.name }}</h5>
-                </div>
-                <div class="d-flex justify-content-around align-items-center w-100">
-                    <p class="card-text m-0">
-                        Indirizzo: <b>{{ item.address }}</b>
-                    </p>
-                </div>
-                <div class="text-center mt-3">
-                    <span v-for="(item, index) in item.types" :key="index"
-                        class="badge rounded-pill bg-primary text-light p-1 mx-1">
-                        <img :src="'../src/assets/' + item.img + '.svg'" class="svg-pill">
-                    </span>
-                </div>
-                <router-link :to="{ name: 'restaurant-menu', params: { id: item.id } }" class="btn btn-primary mt-3">
-                    <font-awesome-icon icon="fa-solid fa-circle-info" class="me-2" />
-                    <span>Menu</span>
-                </router-link>
+
+
+            <div class="w-100 mt-2">
+                <h5 class="card-title text-center restaurant-name fs-4">{{ item.name }}</h5>
             </div>
+
+
+            <div class="text-center mt-3 d-flex flex-wrap justify-content-center w-100">
+                <span v-for="(item, index) in item.types" :key="index" class="pill">
+                    <img :src="'../src/assets/' + item.img + '.svg'" class="svg-pill">
+                </span>
+            </div>
+            <router-link :to="{ name: 'restaurant-menu', params: { id: item.id } }" class="btn btn-warning my-3">
+                <font-awesome-icon icon="fa-solid fa-utensils" class="me-2" />
+                <span>Menu</span>
+            </router-link>
         </div>
     </div>
 </template>
 
 <style scoped>
+.btn {
+    width: 100px;
+}
+
 .card {
+    width: 416px !important;
+    height: 430px !important;
     border-radius: 32px;
     background-color: #9df2e9;
     color: #006a64;
     border: 0;
     box-shadow: 0px 6px 10px 0px rgba(133, 133, 133, 0.5);
+    overflow: hidden;
 }
 
 .card-title {
@@ -58,17 +62,28 @@ export default {
 .card-img-container {
     cursor: pointer;
     border-radius: 32px 32px 0 0;
+    /* overflow: hidden; */
+    min-height: 0 !important;
+    max-height: 60% !important;
     overflow: hidden;
+    object-fit: cover;
+    object-position: center;
+}
+
+.card-body {
+    padding: 0 !important;
+    min-height: 30% !important;
+    max-height: 100% !important;
 }
 
 .card-img-top {
-    width: 100%;
-    height: 200px;
+    min-width: 100% !important;
+    min-height: 100% !important;
     object-fit: cover;
     object-position: center;
 }
 
 .svg-pill {
-    width: 4rem;
+    max-width: 80px;
 }
 </style>
